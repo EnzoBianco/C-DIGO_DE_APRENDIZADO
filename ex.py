a43e1d7e757f9a3ce617fcacd8286c678ad91120
EnzoBianco <enzoa692@gmail.com>
2025-03-25 15:16:49 -0300

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,7 +1,7 @@
-import locate
 import os
 import emoji
-import colorama
+from colorama import Fore, Back, Style, init
+init(autoreset=True)
 
 num1 = int(input("Digite o primeiro número inteiro: "))
 num2 = int(input("Digite o segundo número inteiro: "))
@@ -47,12 +47,25 @@ elif primeiro_numero == terceiro_numero and primeiro_numero != segundo_numero an
     print("O primeiro e terceiro números digitados são iguais.")
 elif primeiro_numero == quarto_numero and primeiro_numero != segundo_numero and primeiro_numero != terceiro_numero:
     print("O primeiro e quarto números digitados são iguais.")
+elif primeiro_numero == segundo_numero and primeiro_numero == terceiro_numero and primeiro_numero == quarto_numero:
+    print("Todos os números digitados são iguais.")
+elif primeiro_numero == segundo_numero and primeiro_numero == terceiro_numero and primeiro_numero != quarto_numero:
+    print("O primeiro, segundo e terceiro números digitados são iguais.")
+elif primeiro_numero == segundo_numero and primeiro_numero == quarto_numero and primeiro_numero != terceiro_numero:
+    print("O primeiro, segundo e quarto números digitados são iguais.")
+elif primeiro_numero == quarto_numero and primeiro_numero == terceiro_numero and primeiro_numero != segundo_numero:
+    print("O primeiro, terceiro e quarto números digitados são iguais.")
 elif segundo_numero == terceiro_numero and segundo_numero != primeiro_numero and segundo_numero != quarto_numero:
     print("O segundo e terceiro números digitados são iguais.")
 elif segundo_numero == quarto_numero and segundo_numero != primeiro_numero and segundo_numero != terceiro_numero:
     print("O segundo e quarto números digitados são iguais.")
-else:
+elif segundo_numero == terceiro_numero and segundo_numero == quarto_numero and segundo_numero != primeiro_numero:
+    print("O segundo, terceiro e quarto números digitados são iguais.")
+elif terceiro_numero == quarto_numero and terceiro_numero != primeiro_numero and terceiro_numero != segundo_numero:
     print("O terceiro e quarto números digitados são iguais.")
+else:
+    print("Nenhum dos números digitados são iguais.")
+
     
 os.system("pause")
     
@@ -108,48 +121,46 @@ os.system("cls")
 
 print("Temos dois tipos de apartamento para você e sua família!")
 
-print("Apartamento 1 - Mais simplicidade, porém com muito conforto!")
-print("Apartamento 2 - Mais luxo para você e sua família, com a melhor expêriencia!") 
+print(f"{Style.BRIGHT + Back.WHITE}Apartamento 1{Style.RESET_ALL} - Mais simplicidade, porém com muito conforto!")
+print(f"{Style.BRIGHT + Back.WHITE}Apartamento 2{Style.RESET_ALL} - Mais luxo para você e sua família, com a melhor expêriencia!") 
         
         
 escolha = input("Digite o número do apartamento que deseja (1 ou 2): ")
 
 if escolha == "1":
-    print("Você escolheu o Apartamento 1 - Mais simplicidade, porém com muito conforto!")
+    print("Ótima escolha! Você escolheu o Apartamento 1 - Mais simplicidade, porém com muito conforto!")
     print("Digite, por favor, a quantidade de pessoas, contando com você, que estarão na colônia de férias: ")
     quantidade = int(input())
-    print("Ótima escolha!")
     if quantidade == 1:
-        print("Pelos seis dias, o total a ser pago é de R$120,00.")
+        print(f"Pelos seis dias, o total a ser pago é de {Fore.GREEN}R$120,00{Fore.RESET}.")
     elif quantidade == 2:
-        print("Pelos seis dias, o total a ser pago é de R$168,00.")
+        print(f"Pelos seis dias, o total a ser pago é de {Fore.GREEN}R$168,00{Fore.RESET}.")
     elif quantidade == 3:
-        print("Pelos seis dias, o total a ser pago é de R$210,00.")
+        print(f"Pelos seis dias, o total a ser pago é de {Fore.GREEN}R$210,00{Fore.RESET}.")
     elif quantidade == 4:
-        print("Pelos seis dias, o total a ser pago é de R$252,00.")
+        print(f"Pelos seis dias, o total a ser pago é de {Fore.GREEN}R$252,00{Fore.RESET}.")
     elif quantidade == 5:
-        print("Pelos seis dias, o total a ser pago é de R$240,00.")
+        print(f"Pelos seis dias, o total a ser pago é de {Fore.GREEN}R$240,00{Fore.RESET}.")
     elif quantidade == 6:
-        print("Pelos seis dias, o total a ser pago é de R$318,00.")
+        print(f"Pelos seis dias, o total a ser pago é de {Fore.GREEN}R$318,00{Fore.RESET}.")
     else:
         print("Número de pessoas inválido.")   
         
 elif escolha == "2":
-    print("Você escolheu o Apartamento 2 - Mais luxuoso para você e sua família, com a melhor experiência!")
+    print("Ótima escolha! Você escolheu o Apartamento 2 - Mais luxuoso para você e sua família, com a melhor experiência!")
     print("Digite, por favor, a quantidade de pessoas, contando com você, que estarão na colônia de férias: ")
     quantidade = int(input())
-    print("Ótima escolha!")
     if quantidade == 1:
-        print("Pelos seis dias, o total a ser pago é de R$150,00.")
+        print(f"Pelos seis dias, o total a ser pago é de {Fore.GREEN}R$150,00{Fore.RESET}.")
     elif quantidade == 2:
-        print("Pelos seis dias, o total a ser pago é de R$204,00.")
+        print(f"Pelos seis dias, o total a ser pago é de {Fore.GREEN}R$204,00{Fore.RESET}.")
     elif quantidade == 3:
-        print("Pelos seis dias, o total a ser pago é de R$252,00.")
+        print(f"Pelos seis dias, o total a ser pago é de {Fore.GREEN}R$252,00{Fore.RESET}.")
     elif quantidade == 4:
-        print("Pelos seis dias, o total a ser pago é de R$300,00.")
+        print(f"Pelos seis dias, o total a ser pago é de {Fore.GREEN}R$300,00{Fore.RESET}.")
     elif quantidade == 5:
-        print("Pelos seis dias, o total a ser pago é de R$342,00.")
+        print(f"Pelos seis dias, o total a ser pago é de {Fore.GREEN}R$342,00{Fore.RESET}.")
     elif quantidade == 6:
-        print("Pelos seis dias, o total a ser pago é de R$378,00.")
+        print(f"Pelos seis dias, o total a ser pago é de {Fore.GREEN}R$378,00{Fore.RESET}.")
     else:
-        print("Número de pessoas inválido.")
+        print(Fore.RED + "Número de pessoas inválido.")
